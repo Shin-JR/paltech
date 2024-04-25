ea00a8de12c734cfe3c85dccfd9d6454e09fd881
Shin-JR <j.riquelme@uc.cl>
2024-04-25 14:53:19 -0400

    Update CSS styles in Header, common/index, Map, and App components

--- src/components/common/App.jsx
+++ src/components/common/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import Map from "../map/Map";
+import { useState } from "react";
 
 function App() {
+  const [geofences, setGeofences] = useState([])
+  
+
   return (
-    <div>
-      <Map/>
+    <div className="main-container">
+      <div className="app-container">
+        <div className="sidebar">
+          <h1>hola</h1>
+        </div>
+        <div className="map-div">
+          <Map/>
+        </div>
+      </div>
     </div>
   );
 }
