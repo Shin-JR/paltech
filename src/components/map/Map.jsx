d5fda7e3da62c079d6d2c7ec6fcca05160239ec1
Shin-JR <j.riquelme@uc.cl>
2024-04-25 16:28:06 -0400

    Refactor Map component to handle geofence click events and add markers

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -9,9 +9,29 @@ import {EditControl} from "react-leaflet-draw";
 // based on https://github.com/codegeous/react-component-depot/blob/master/src/pages/Leaflet/polygon.js
 export default function Map({geofences, setGeofences}) {
   const [center, setCenter] = useState([-33.4372, -70.6506])
-  const ZOOM_LEVEL = 12;
+  // const [center, setCenter] = useState([47.7364, 10.3235])
+  const ZOOM_LEVEL = 15;
+
+  const _onMapClick = (e) => {
+    const { latlng } = e;
+    
+    // Verificar si el clic está dentro de algún geofence
+    const isInsideGeofence = geofences.some((geofence) => {
+      const polygon = L.polygon(geofence.latlngs);
+      return polygon.contains(latlng);
+    });
+
+    if (isInsideGeofence) {
+      // Permitir agregar el marcador
+      // Aquí puedes agregar lógica para agregar el marcador en el estado o hacer lo que necesites
+      console.log("Dentro de un geofence");
+    } else {
+      console.log("Fuera de un geofence");
+    }
+  };
+
   const _onCreated = (e) => {
-    console.log(e);
+    // console.log(e);
 
     const { layerType, layer } = e;
     if (layerType === "polygon") {
@@ -19,13 +39,13 @@ export default function Map({geofences, setGeofences}) {
 
       setGeofences((layers) => [
         ...layers,
-        { id: _leaflet_id, latlngs: layer.getLatLngs()[0] },
+        { id: _leaflet_id, latlngs: layer.getLatLngs()[0], markers: [] },
       ]);
     }
   };
 
   const _onEdited = (e) => {
-    console.log(e);
+    // console.log(e);
     const {
       layers: { _layers },
     } = e;
@@ -58,6 +78,7 @@ export default function Map({geofences, setGeofences}) {
         center={ center }
         zoom={ZOOM_LEVEL}
         style={{ height: "100%"}}
+        onClick={_onMapClick}
       >
         <FeatureGroup>
           <EditControl
@@ -89,9 +110,9 @@ export default function Map({geofences, setGeofences}) {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[-33.4372, -70.6506]}>
+        <Marker position={center}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            Paltech
           </Popup>
         </Marker>
       </MapContainer>
